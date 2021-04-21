a3f03fb6014ca20fd59def2cbada19b3cea1c68e
Alex Semchenkov <zigola12@list.ru>
2021-04-21 15:49:08 +0300

    Made prepare Camera

---
+++ Assets/Match3/Scripts/CameraAspect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Match3
+{
+    [ExecuteAlways]
+    public class CameraAspect : MonoBehaviour
+    {
+        [SerializeField]
+        float width;
+
+        Camera targetCamera;
+
+        void Awake()
+        {
+            targetCamera = GetComponent<Camera>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            targetCamera.orthographicSize = (float)Screen.height / Screen.width * width;
+        }
+    }
+}
